cd8f4bcf8b54dcf8ef7ed7dd34e93c7fd1635e5f
Alfredo Braule <alfredoneto934@gmail.com>
2021-05-25 23:23:33 -0300

    ft_strlcpy added, ready to test

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,14 +6,30 @@
 /*   By: ade-agui <ade-agui@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 21:54:02 by ade-agui          #+#    #+#             */
-/*   Updated: 2021/05/25 23:00:40 by ade-agui         ###   ########.fr       */
+/*   Updated: 2021/05/25 23:22:03 by ade-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 size_t  strlcpy(char *dst, const char *src, size_t size)
-{
-    
+{   
+    size_t i;
+    size_t srcsize;
+
+    i = 0;
+    while (!dst || !src)
+        return (0);
+    srcsize = ft_strlen(src);
+    if (size != 0)
+    {
+        while (src[i] != '\0' && i < (size - 1))
+        {
+            dst[i] = src[i];
+            i++;
+        }
+        dst[i] = '\0';
+    }
+    return (srcsize);
 }
 
