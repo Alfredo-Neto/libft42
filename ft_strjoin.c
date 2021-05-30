d9ca1d6ed524fc294f743bee879a954c0f7d1caf
Alfredo Braule <alfredoneto934@gmail.com>
2021-05-29 21:51:52 -0300

    tested norminette in all files

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,27 +6,22 @@
 /*   By: ade-agui <ade-agui@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/29 18:08:34 by ade-agui          #+#    #+#             */
-/*   Updated: 2021/05/29 21:33:11 by ade-agui         ###   ########.fr       */
+/*   Updated: 2021/05/29 21:45:18 by ade-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/**
- *  Allocates (with malloc(3)) and returns a new
- *  string, which is the result of the concatenation
- *  of ’s1’ and ’s2’.
- */
-
 #include "libft.h"
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-    char 	*join;
-    size_t len;
-    size_t i;
-    size_t j;
+	char	*join;
+	size_t	len;
+	size_t	i;
+	size_t	j;
 
-    len = (ft_strlen(s1) + ft_strlen(s2)) + 1;
-    if (!s1 || !s2 || !(join = (char *)malloc(len))) return (0);
+	len = (ft_strlen(s1) + ft_strlen(s2)) + 1;
+	if (!s1 || !s2 || !(join = (char *)malloc(len)))
+		return (0);
 	i = 0;
 	while (s1[i] != '\0')
 	{
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,30 +6,29 @@
 /*   By: ade-agui <ade-agui@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 21:54:02 by ade-agui          #+#    #+#             */
-/*   Updated: 2021/05/25 23:39:58 by ade-agui         ###   ########.fr       */
+/*   Updated: 2021/05/29 21:50:15 by ade-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t  ft_strlcpy(char *dst, const char *src, size_t size)
-{   
-    size_t i;
-    size_t srcsize;
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	srcsize;
 
-    i = 0;
-    while (!dst || !src)
-        return (0);
-    srcsize = ft_strlen(src);
-    if (size != 0)
-    {
-        while (src[i] != '\0' && i < (size - 1))
-        {
-            dst[i] = src[i];
-            i++;
-        }
-        dst[i] = '\0';
-    }
-    return (srcsize);
+	i = 0;
+	while (!dst || !src)
+		return (0);
+	srcsize = ft_strlen(src);
+	if (size != 0)
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (srcsize);
 }
-
