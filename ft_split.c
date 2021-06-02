6f0ffb10a07dd4aba7875449e4d3d6ef98135367
Alfredo Braule <alfredoneto934@gmail.com>
2021-06-02 05:37:24 -0300

    tested and fixed ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: ade-agui <ade-agui@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 15:57:05 by ade-agui          #+#    #+#             */
-/*   Updated: 2021/06/02 05:20:55 by ade-agui         ###   ########.fr       */
+/*   Updated: 2021/06/02 05:35:21 by ade-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,20 @@ static int count_substrings(char const *s, char c)
 {
 	int i;
 	int n_word;
-	int control;
+	int key;
 
 	i = 0;
-	control = 0;
-	while (s[i++] != '\0' && s != '\0')
+	key = 0;
+	n_word = 0;
+	while (s[i++] != '\0' && *s != '\0')
 	{
-		if (s[i] != c && control == 0)
+		if (s[i] != c && key == 0)
 		{
-			control = 1;
+			key = 1;
 			n_word++;
 		}
 		else if (s[i] == c)
-			control = 0;
+			key = 0;
 	}
 	return (n_word);
 }
@@ -43,7 +44,7 @@ static char *substring(char const *s, int begin, int end)
 	if (!(substring = (char *)malloc(len)))
 		return (NULL);
 	i = 0;
-	while (end > begin)
+	while (begin < end)
 		substring[i++] = s[begin++];
 	substring[i] = '\0';
 	return (substring);
@@ -63,14 +64,17 @@ char **ft_split(char const *s, char c)
 	j = 0;
 	init_w = -1;
 	len = count_substrings(s, c) + 1 * sizeof(char *);
-	if (!(split = (char *)malloc(len)))
+	if (!(split = (char **)malloc(len)))
 		return (NULL);
-	while (s[++i] != '\0')
+	while (++i <= ft_strlen(s))
 	{
 		if (s[i] != c && init_w < 0)
 			init_w = i;
-		else if ((s[i] == c || s[i] == '\0') && init_w >= 0)
+		else if ((s[i] == c || i == ft_strlen(s)) && init_w >= 0)
+		{
 			split[j++] = substring(s, init_w, i);
+			init_w = -1;
+		}	
 	}
 	split[j] = NULL;
 	return (split);
