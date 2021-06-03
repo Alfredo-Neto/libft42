bd976aa240b21d5790797e10264455602b9dd2d9
Alfredo Braule <alfredoneto934@gmail.com>
2021-06-03 06:54:49 -0300

    implemented ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: ade-agui <ade-agui@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 05:53:48 by ade-agui          #+#    #+#             */
-/*   Updated: 2021/06/02 05:54:08 by ade-agui         ###   ########.fr       */
+/*   Updated: 2021/06/03 06:07:45 by ade-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,27 @@
 
 char	*ft_itoa(int n)
 {
-    
+	char	*res;
+	int		len;
+	int		signal;
+
+	len = n <= 0 ? 1 : 0;
+	while (n != 0)
+	{
+		n /= 10;
+		len++;
+	}
+	signal = n < 0 ? -1 : 1;
+	if (!(res = (char *)malloc((len + 1) * sizeof(char))))
+		return (0);
+	n = n < 0 ? -n : n;
+	res[len--] = '\0';
+	while (len >= 0)
+	{
+		res[len--] = (n % 10) + '0';
+		n /= 10;
+	}
+	if (signal == -1)
+		res[0] = '-';
+	return (res);
 }
