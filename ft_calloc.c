eb8929ff396a407865683d70710d95516da5c46e
Alfredo Braule <alfredoneto934@gmail.com>
2021-06-05 07:08:43 -0300

    fixed norme on ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ade-agui <ade-agui@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 09:09:06 by ade-agui          #+#    #+#             */
-/*   Updated: 2021/05/28 22:57:24 by ade-agui         ###   ########.fr       */
+/*   Updated: 2021/06/05 07:07:57 by ade-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*dst;
 
 	tot_size = size * nmemb;
-	if (!(dst = malloc(tot_size)))
+	dst = malloc(tot_size);
+	if (dst == 0)
 		return (0);
 	ft_memset(dst, 0, tot_size);
 	return (dst);
