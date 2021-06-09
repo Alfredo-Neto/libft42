897de71ecb82b04c2c757e7efe2ea85f363404c9
Alfredo Braule <alfredoneto934@gmail.com>
2021-06-09 05:14:28 -0300

    implemented ft_lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -1,19 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ade-agui <ade-agui@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/09 02:05:39 by ade-agui          #+#    #+#             */
-/*   Updated: 2021/06/09 04:38:07 by ade-agui         ###   ########.fr       */
+/*   Created: 2021/06/09 03:33:42 by ade-agui          #+#    #+#             */
+/*   Updated: 2021/06/09 05:13:58 by ade-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	new->next = *lst;
-	*lst = new;
+	t_list	*last;
+	
+	last = ft_lstlast(*lst);
+	if (last != NULL)
+		last->next = new;
+	else
+		*lst = new;
 }
