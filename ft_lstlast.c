a410333ac67eea9fcd0a85c927123131222de5a6
Alfredo Braule <alfredoneto934@gmail.com>
2021-06-09 03:33:12 -0300

    implemented ft_lstlast

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ade-agui <ade-agui@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/09 03:15:38 by ade-agui          #+#    #+#             */
-/*   Updated: 2021/06/09 03:25:06 by ade-agui         ###   ########.fr       */
+/*   Created: 2021/06/09 03:25:09 by ade-agui          #+#    #+#             */
+/*   Updated: 2021/06/09 03:32:47 by ade-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_lstsize(t_list *lst)
+t_list	*ft_lstlast(t_list *lst)
 {
-	int	i;
-
-	if (lst == NULL)
+	if(lst == NULL)
 		return (0);
-	i = 1;
 	while (lst->next != NULL)
-	{
 		lst = lst->next;
-		i++;
-	}
-	return (i);
+	return (lst);
 }
